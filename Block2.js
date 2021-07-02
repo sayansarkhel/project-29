7ff4d6471dab301e7df8cb0ffed5cb0e6c551403
sayansarkhel <76253744+sayansarkhel@users.noreply.github.com>
2021-07-02 11:20:45 +0530

    Update Block2.js

--- Block2.js
+++ Block2.js
@@ -1,9 +1,9 @@
 class Block2{
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            'restitution':0.4,
+            'friction':0.0,
+            
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
